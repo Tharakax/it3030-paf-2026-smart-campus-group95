50c06d6a61ac8fec8ee9e9b4e5554509d670f574
unknown <it23841086@my.sliit.lk>
2026-04-12 23:32:06 +0530

    Remove ResourceStatus enum
    Delete com.unisync.enums.ResourceStatus from the backend, removing the enum that defined ACTIVE and OUT_OF_SERVICE. This cleans up an unused/obsolete resource status definition from the codebase.

--- backend/src/main/java/com/unisync/enums/ResourceStatus.java
+++
@@ -1,9 +0,0 @@
-package com.unisync.enums;
-
-/**
- * Enum representing the current status of a campus resource in the Facilities & Assets Catalogue.
- */
-public enum ResourceStatus {
-    ACTIVE,
-    OUT_OF_SERVICE
-}
